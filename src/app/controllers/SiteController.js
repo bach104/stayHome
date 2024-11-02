ad04d768180079628c13f9d9f7c138d7a7f1810f
bach104 <114276890+bach104@users.noreply.github.com>
2024-11-02 14:25:19 +0700

    Refacter code: create route,app/controllers folders

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,9 +1,9 @@
 class SiteController {
-  home(req, res) {
+  index(req, res) {
     res.render('home');
   }
   search(req, res) {
     res.render('search');
   }
 }
-module.exports = new SiteController;
+module.exports = new SiteController; 
--- src/routes/news.js
+++ src/routes/news.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
 const newsController = require('../app/controllers/NewControllers');
-
 router.use('/:slug', newsController.show);
 router.use('/', newsController.index);
-
 module.exports = router;
