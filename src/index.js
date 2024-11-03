7d2f6b6bbd88b1fd671ac01ad04004121dc42ddf
bach104 <114276890+bach104@users.noreply.github.com>
2024-11-03 09:19:34 +0700

    'fix'

--- src/index.js
+++ src/index.js
@@ -1,28 +1,37 @@
-const path =require('path')
-const handlebars = require('express-handlebars')
-const express = require('express')
-const morgan = require('morgan')
-const app = express()
-const port = 3000
-const route = require('./routes')
+const path = require('path');
+const handlebars = require('express-handlebars');
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
+const route = require('./routes');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.json()); 
- 
+app.use(express.json());
+
 app.use(morgan('combined'));
 
-app.engine('hbs', handlebars.engine({
-  extname: '.hbs',
-}));
+app.engine(
+    'hbs',
+    handlebars.engine({
+        extname: '.hbs',
+    }),
+);
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resource/views'));
 
 route(app);
- 
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+    console.log(`Example app listening at http://localhost:${port}`);
+});
+
+// prettier : đẹp hơn
+
+// lint-staged: xơ vải dàn dựng
+
+// husky: hành động ,cam kết
